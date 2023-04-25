601b3a940a93fd8c8d5d8c4e161d5de40068af79
Stephane BASSET <sb909147@iut21.u-bourgogne.fr>
2023-04-25 17:21:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a2022_r411_meteo_basset_stephane/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2022_r411_meteo_basset_stephane;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
